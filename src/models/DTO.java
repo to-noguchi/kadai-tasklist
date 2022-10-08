9c60edb167d2c841e682ef6eb19a93bb5669bca3
tomoyo.noguchi <azalea5.hydrangea6@gmail.com>
2022-10-08 23:34:10 +0900

    lesson16課題：テーブルの作成、「0」表示正常

--- src/models/DTO.java
+++ src/models/DTO.java
@@ -7,10 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+
 @Entity
-@Table(name = "messages")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAlltasks",
+        query = "SELECT m FROM DTO AS m ORDER BY m.id DESC"
+    )
+})
+@Table(name = "tasks")
 public class DTO {
     @Id
     @Column(name = "id")
